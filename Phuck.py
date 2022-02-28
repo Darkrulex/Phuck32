41f332155b68304a9cfca44b1efd24d9120f1473
Qaiser Abbas <69212320+TechQaiser@users.noreply.github.com>
2022-02-28 23:28:49 +0500

    Create Phuck.py

---
+++ Phuck.py
@@ -0,0 +1,33 @@
+import os, platform
+
+try:
+
+   import requests
+
+except:
+
+   os.system('pip2 install requests')
+
+   import mechanize
+
+except:
+
+   os.system('pip2 install mechanize')
+
+import requests
+
+bit = platform.architecture()[0]
+
+elif bit == '64bit':
+
+    import Phuck
+
+    Phuck. __techq()
+
+elif bit == '32bit':
+
+    print('bro its just for 32bit device')
+
+    print('you can use this command \n git clone https://github.com/TechQaiser/Phuck')
+
+    print('cd Phuck , python Phuck.py')
