d07b0c05cddabb1e38ab369552f7b94c59045daf
Qaiser Abbas <69212320+TechQaiser@users.noreply.github.com>
2022-02-28 23:31:15 +0500

    Update Phuck.py

--- Phuck.py
+++ Phuck.py
@@ -18,16 +18,11 @@ import requests
 
 bit = platform.architecture()[0]
 
-elif bit == '64bit':
+elif bit == '32bit':
 
     import Phuck
 
     Phuck. __techq()
 
-elif bit == '32bit':
 
-    print('bro its just for 32bit device')
 
-    print('you can use this command \n git clone https://github.com/TechQaiser/Phuck')
-
-    print('cd Phuck , python Phuck.py')
