991abde0875a57413a45b981d6fe788d0eccac61
Qaiser Abbas <69212320+TechQaiser@users.noreply.github.com>
2022-02-28 23:50:56 +0500

    Update Phuck.py

--- Phuck.py
+++ Phuck.py
@@ -1,10 +1,8 @@
 import os, platform
-
 import requests
 
-
-bit = platform.architecture()[0]import os, platform
+bit = platform.architecture()[0]
+import os, platform
 if bit == '32bit':
     from Phuck import __techq
     __techq()
-    
