659a51cb9c8b547a7f96ba7487821bc8e7c452db
Qaiser Abbas <69212320+TechQaiser@users.noreply.github.com>
2022-02-28 23:35:54 +0500

    Update Phuck.py

--- Phuck.py
+++ Phuck.py
@@ -8,14 +8,6 @@ except:
 
    os.system('pip2 install requests')
 
-   import mechanize
-
-except:
-
-   os.system('pip2 install mechanize')
-
-import requests
-
 bit = platform.architecture()[0]
 
 if bit == '32bit':
